7947478befc03f37ab9171d3cbffdc3a387a4bac
BartoszDabek <bartek50600@wp.pl>
2017-03-02 14:03:18 +0100

    edited settingsButton to reactOnClick inside class

--- core/src/pl/devpress/refpress/ui/SettingsButton.java
+++ core/src/pl/devpress/refpress/ui/SettingsButton.java
@@ -1,13 +1,31 @@
 package pl.devpress.refpress.ui;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
+import pl.devpress.refpress.RefPress;
 
 public class SettingsButton extends Image {
 	
 	public static final String settingsButtonImg = "settingsbutton.png";
-
-	public SettingsButton() {
+	private RefPress game;
+	
+	public SettingsButton(RefPress game) {
 		super(new Texture(settingsButtonImg));
+		this.game = game;
+		
+		reactOnClick();
+	}
+
+	private void reactOnClick() {
+		this.addListener(new ClickListener(){
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+				System.out.println("settings");
+				return super.touchDown(event, x, y, pointer, button);
+			}
+		});
 	}
 }
