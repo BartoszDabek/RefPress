f2509428362d87f410e78cd01cdcdc638a6e302a
BartoszDabek <bartek50600@wp.pl>
2017-03-02 12:38:42 +0100

    edited playButton class to work with callBack interface

--- core/src/pl/devpress/refpress/ui/PlayButton.java
+++ core/src/pl/devpress/refpress/ui/PlayButton.java
@@ -1,14 +1,28 @@
 package pl.devpress.refpress.ui;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class PlayButton extends Image {
 	
 	public static final String playButtonImg = "playbuttonx200.png";
 	
-	public PlayButton() {
+	public PlayButton(final IClickCallBack callback) {
 		super(new Texture(playButtonImg));
+		
+		init(callback);
+	}
+
+	private void init(final IClickCallBack callback) {
+		this.addListener(new ClickListener(){
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+				callback.onClick();
+				return super.touchDown(event, x, y, pointer, button);
+			}
+		});
 	}
 
 
