7c1cb60e25af96d35d1f48ce4f959a3e802a0804
BartoszDabek <bartek50600@wp.pl>
2017-03-02 10:07:42 +0100

    edited menuscreen

--- core/src/pl/devpress/refpress/screens/MenuScreen.java
+++ core/src/pl/devpress/refpress/screens/MenuScreen.java
@@ -1,48 +1,59 @@
 package pl.devpress.refpress.screens;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 import pl.devpress.refpress.RefPress;
+import pl.devpress.refpress.ui.PlayButton;
+import pl.devpress.refpress.ui.SettingsButton;
 
 public class MenuScreen extends AbstractScreen {
 	
-	private final String playButtonImg = "playbutton.png";
 	
-	private Texture playButton;
+	private PlayButton playButton;
+	private SettingsButton settingsButton;
+	private Table table;
 	
 	public MenuScreen(RefPress game) {
 		super(game);
-		playButton = new Texture(playButtonImg);
-		
 	}
 
 	@Override
 	protected void init() {
+		playButton = new PlayButton();
+		settingsButton = new SettingsButton();
+		playButton.addListener(new ClickListener() {
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+				
+				game.setScreen(new PlayScreen(game));
+				dispose();
+				
+				return super.touchDown(event, x, y, pointer, button);
+			}
+			
+		});
+		
+		
+		table = new Table();
+		table.setFillParent(true);
+		stage.addActor(table);
+	//	table.setDebug(true);
+		table.add(playButton).width(230).height(230).pad(30);
+		table.top().padTop(50);
+		table.row();
+		table.add(settingsButton).width(200).height(200).pad(30);
 	}
 
 	@Override
 	public void render(float delta) {
 		super.render(delta);
 		
-		if(Gdx.input.justTouched()) {
-			game.setScreen(new PlayScreen(game));
-			dispose();
-		}
-		
 		spriteBatch.begin();
-		spriteBatch.draw(playButton, getHalfOfXAxis(), getHalfOfYAxis());
+		stage.draw();
 		spriteBatch.end();
 	}
-	
-	
-	
-	private float getHalfOfYAxis() {
-		return (RefPress.HEIGHT/2) - (playButton.getHeight()/2);
-	}
 
-	private float getHalfOfXAxis() {
-		return (RefPress.WIDTH/2) - (playButton.getWidth()/2);
-	}
 	
 }
