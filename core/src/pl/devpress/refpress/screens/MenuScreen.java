387c6337e37f3de82a974d123b062bbf55e55a96
BartoszDabek <bartek50600@wp.pl>
2017-03-02 12:39:30 +0100

    edited menuScreen class to work with interface + refactor class a little

--- core/src/pl/devpress/refpress/screens/MenuScreen.java
+++ core/src/pl/devpress/refpress/screens/MenuScreen.java
@@ -1,10 +1,9 @@
 package pl.devpress.refpress.screens;
 
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 import pl.devpress.refpress.RefPress;
+import pl.devpress.refpress.ui.IClickCallBack;
 import pl.devpress.refpress.ui.PlayButton;
 import pl.devpress.refpress.ui.SettingsButton;
 
@@ -21,29 +20,40 @@ public class MenuScreen extends AbstractScreen {
 
 	@Override
 	protected void init() {
-		playButton = new PlayButton();
-		settingsButton = new SettingsButton();
-		playButton.addListener(new ClickListener() {
-			@Override
-			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-				
-				game.setScreen(new PlayScreen(game));
-				dispose();
-				
-				return super.touchDown(event, x, y, pointer, button);
-			}
-			
-		});
-		
-		
-		table = new Table();
-		table.setFillParent(true);
-		stage.addActor(table);
-	//	table.setDebug(true);
+		initPlayButton();		
+		initSettingsButton();
+		initTable();
+		addElementToTable();
+
+	}
+
+
+	private void addElementToTable() {
 		table.add(playButton).width(230).height(230).pad(30);
 		table.top().padTop(50);
 		table.row();
 		table.add(settingsButton).width(200).height(200).pad(30);
+	}
+
+	private void initTable() {
+		table = new Table();
+		table.setFillParent(true);
+		table.setDebug(true);
+		stage.addActor(table);
+	}
+
+	private void initSettingsButton() {
+		settingsButton = new SettingsButton();
+	}
+	
+	private void initPlayButton() {
+			playButton = new PlayButton(new IClickCallBack() {
+				
+			@Override
+			public void onClick() {
+				game.setScreen(new PlayScreen(game));
+			}
+		});
 	}
 
 	@Override
