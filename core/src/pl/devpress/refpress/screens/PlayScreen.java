570718e7ed5e718a491b63998dd431a391d53cca
BartoszDabek <bartek50600@wp.pl>
2017-03-07 15:40:17 +0100

    deleted table

--- core/src/pl/devpress/refpress/screens/PlayScreen.java
+++ core/src/pl/devpress/refpress/screens/PlayScreen.java
@@ -1,15 +1,11 @@
 package pl.devpress.refpress.screens;
 
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-
 import pl.devpress.refpress.RefPress;
 import pl.devpress.refpress.ui.RedDot;
 
 public class PlayScreen extends AbstractScreen {
 
 	private RedDot redDot;
-	private Table table;
 	
 	public PlayScreen(RefPress game) {
 		super(game);
@@ -17,24 +13,15 @@ public class PlayScreen extends AbstractScreen {
 
 	@Override
 	protected void init() {
-		initRedDot();
-		initTable();
-		addElementToTable();
+			initRedDot();
 	}
 	
-	private void addElementToTable() {
-		table.add(redDot);
-	}
 
-	private void initTable() {
-		table = new Table();
-		table.setFillParent(true);
-		table.setDebug(true);
-		stage.addActor(table);
-	}
 
 	private void initRedDot() {
-		redDot = new RedDot(game);
+		redDot = new RedDot(stage);
+		stage.addActor(redDot);
+		stage.setDebugAll(true);
 	}
 
 	@Override
