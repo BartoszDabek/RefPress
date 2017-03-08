37942213c03fa8eb8d84e1a34c8e60d1ba5d8c74
BartoszDabek <bartek50600@wp.pl>
2017-03-08 14:05:47 +0100

    deleted reactOnClick, moved that to gameDotService

--- core/src/pl/devpress/refpress/ui/RedDot.java
+++ core/src/pl/devpress/refpress/ui/RedDot.java
@@ -1,10 +1,7 @@
 package pl.devpress.refpress.ui;
 
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 import pl.devpress.refpress.RefPress;
 
@@ -14,27 +11,13 @@ public class RedDot extends Image{
 	private final static int WIDHT = 50;
 	private final static int HEIGHT = 50;
 	
-	public RedDot(Stage stage) {
+	public RedDot() {
 		super(new Texture(redDotImg));
 		
 		this.setOrigin(WIDHT / 2, HEIGHT / 2);
 		this.setSize(WIDHT, HEIGHT);	
 		this.setPosition(getHalfOfXAxis(),getHalfOfYAxis());
 		
-		
-		reactOnClick(stage);
-	}
-
-	private void reactOnClick(final Stage stage) {
-		this.addListener(new ClickListener(){
-			@Override
-			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-				System.out.println("click");
-				RedDot redDot = new RedDot(stage);
-				stage.addActor(redDot);
-				return super.touchDown(event, x, y, pointer, button);
-			}
-		});
 	}
 
 	private float getHalfOfYAxis() {
