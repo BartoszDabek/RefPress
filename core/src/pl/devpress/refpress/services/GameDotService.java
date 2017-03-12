8d8133b6399169403c8405ebdc2aaa43f1f696a9
BartoszDabek <bartek50600@wp.pl>
2017-03-12 18:55:27 +0100

    dot shows 5 times until setting screen

--- core/src/pl/devpress/refpress/services/GameDotService.java
+++ core/src/pl/devpress/refpress/services/GameDotService.java
@@ -7,18 +7,23 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Timer;
 import com.badlogic.gdx.utils.Timer.Task;
 
+import pl.devpress.refpress.RefPress;
+import pl.devpress.refpress.screens.ScoreScreen;
 import pl.devpress.refpress.ui.RedDot;
 
 public class GameDotService {
 	
+	private RefPress game;
 	private RedDot redDot;
 	private int timesToRepeat = 0;
 	
-	public GameDotService(Stage stage) {
+	public GameDotService(Stage stage, RefPress game) {
 		init(stage);
+		this.game = game;
 	}
 
 	private void init(final Stage stage) {
+		
 		redDot = new RedDot();
 		stage.addActor(redDot);
 		
@@ -33,6 +38,7 @@ public class GameDotService {
 						if(timesToRepeat >= 4){
 							timesToRepeat = 0;
 							cancel();
+							game.setScreen(new ScoreScreen(game));
 						} else {
 							init(stage);
 							timesToRepeat++;
@@ -48,7 +54,7 @@ public class GameDotService {
 	
 	private float generateRandomSpawnTime() {
 		float start = 0.8f;
-		float end = 4f;
+		float end = 1f;
 		
 		return MathUtils.random(start, end);
 	}
