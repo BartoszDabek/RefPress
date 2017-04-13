4b7bff7a185fd356cb08ab6e4f8f2f36c9f3ce83
BartoszDabek <bartek50600@wp.pl>
2017-04-13 20:45:34 +0200

    added new font and displaying that font on screen

--- core/src/pl/devpress/refpress/screens/ScoreScreen.java
+++ core/src/pl/devpress/refpress/screens/ScoreScreen.java
@@ -1,20 +1,31 @@
 package pl.devpress.refpress.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
 
 import pl.devpress.refpress.RefPress;
 import pl.devpress.refpress.services.GameDotService;
 
 public class ScoreScreen extends AbstractScreen {
 
-	private int screenTouched = 0;
+	private FreeTypeFontGenerator generator;
+	private FreeTypeFontParameter parameter;
+	private BitmapFont textFont;
+	private BitmapFont scoreFont;
+	private BitmapFont decorationFont;
+	private BitmapFont bigText;
 	private String averageReaction;
 	private String slowestReaction;
 	private String fastestReaction;
+	private String averageScore;
+	private String slowestScore;
+	private String fastestScore;
 	private String message;
-	BitmapFont mapFont;
-	
+	private String decoration;
+	private int screenTouched = 0;
 	
 	public ScoreScreen(RefPress game) {
 		super(game);
@@ -22,49 +33,90 @@ public class ScoreScreen extends AbstractScreen {
 
 	@Override
 	protected void init() {
-		displayScore();
+		initFonts();
+		initText();
 	}
 
-	private void displayScore() {
-		averageReaction = "Average reaction: " + GameDotService.getAverageReaction() + "ms";
-		slowestReaction = "Slowest reaction: " + GameDotService.slowestReaction + "ms";
-		fastestReaction = "Fastest reaction: " + GameDotService.fastestReaction + "ms"; 
+	private void initFonts() {
+		generator = new FreeTypeFontGenerator(Gdx.files.internal("OpenSans-ExtraBold.ttf"));
+		parameter = new FreeTypeFontParameter();
+		
+		parameter.size = 24;
+		parameter.borderColor = Color.BLACK;
+		parameter.borderWidth = 2;
+		
+		decorationFont = generator.generateFont(parameter);
+		textFont = generator.generateFont(parameter);
+		scoreFont = generator.generateFont(parameter);
+		
+		parameter.size = 32;
+		parameter.borderWidth = 3;
+		
+		bigText = generator.generateFont(parameter);
+	}
+
+	private void initText() {
+		averageReaction = "Average reaction \n";
+		slowestReaction = "Slowest reaction \n";
+		fastestReaction = "Fastest reaction \n";
 		message = "Tap to retry";
-		mapFont = new BitmapFont();
+		decoration = "__________________";
+		averageScore = GameDotService.getAverageReaction()+ "ms";
+		slowestScore = GameDotService.slowestReaction+ "ms";
+		fastestScore = GameDotService.fastestReaction+ "ms";
 	}
 
 
 	private void setScreen() {
 		if(screenTouched == 2) {
+			resetReactionTimers();
 			game.setScreen(new MenuScreen(game));
-			GameDotService.timesToRepeat = 0;
-			GameDotService.averageReaction = 0;
-			GameDotService.slowestReaction = 0;
-			GameDotService.fastestReaction = 9999999;
 		}
+	}
+	
+	private void resetReactionTimers() {
+		GameDotService.timesRepeated = 0;
+		GameDotService.averageReaction = 0;
+		GameDotService.slowestReaction = 0;
+		GameDotService.fastestReaction = 9999999;
+	}
+
+	private void drawScore() {
+		spriteBatch.begin();
+		textFont.draw(spriteBatch, averageReaction, 130, 600);
+		textFont.draw(spriteBatch, fastestReaction, 130, 460);
+		textFont.draw(spriteBatch, slowestReaction, 130, 320);		
+		bigText.draw(spriteBatch, message, 140, 170);
+		
+		decorationFont.draw(spriteBatch, decoration, 130, 580);
+		decorationFont.draw(spriteBatch, decoration, 130, 540);
+		decorationFont.draw(spriteBatch, decoration, 130, 440);
+		decorationFont.draw(spriteBatch, decoration, 130, 400);
+		decorationFont.draw(spriteBatch, decoration, 130, 300);
+		decorationFont.draw(spriteBatch, decoration, 130, 260);
+
+		scoreFont.draw(spriteBatch, averageScore, 210, 550);
+		scoreFont.draw(spriteBatch, fastestScore, 210, 410);
+		scoreFont.draw(spriteBatch, slowestScore, 210, 270);
+		spriteBatch.end();
 	}
 
 	
 	@Override
 	public void render(float delta) {
 		super.render(delta);
+		drawScore();
 		
 		if(Gdx.input.justTouched()) {
 			screenTouched++;
 			setScreen();
 		}
 		
-		spriteBatch.begin();
-		mapFont.draw(spriteBatch, averageReaction, 150, 410);
-		mapFont.draw(spriteBatch, fastestReaction, 150, 380);
-		mapFont.draw(spriteBatch, slowestReaction, 150, 350);
-		mapFont.draw(spriteBatch, message, 150, 300);
-		spriteBatch.end();
 	}
 	
 	@Override
 	public void dispose() {
-		mapFont.dispose();
+		generator.dispose();
 	}
 	
 }
