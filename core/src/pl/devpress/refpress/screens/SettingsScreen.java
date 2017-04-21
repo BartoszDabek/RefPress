3f655b81d5dc8eaafde516e20a90f6f6770861bf
BartoszDabek <bartek50600@wp.pl>
2017-04-21 14:19:02 +0200

    created settings class

---
+++ core/src/pl/devpress/refpress/screens/SettingsScreen.java
@@ -0,0 +1,15 @@
+package pl.devpress.refpress.screens;
+
+import pl.devpress.refpress.RefPress;
+
+public class SettingsScreen extends AbstractScreen {
+
+	public SettingsScreen(RefPress game) {
+		super(game);
+	}
+
+	@Override
+	protected void init() {
+	}
+
+}
