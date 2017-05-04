31a52b00245169e05c1e84da08716c32c56d5b3f
BartoszDabek <bartek50600@wp.pl>
2017-05-04 16:38:18 +0200

    add textureAtlas loader

--- core/src/pl/devpress/refpress/ui/CheckBox.java
+++ core/src/pl/devpress/refpress/ui/CheckBox.java
@@ -1,5 +1,24 @@
 package pl.devpress.refpress.ui;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import pl.devpress.refpress.RefPress;
+
 public class CheckBox {
+    private static final String checkBoxImg = "checkBox.atlas";
+    private TextureAtlas textureAtlas;
+    private TextureRegion textureRegion;
+    private RefPress game;
+
+    public CheckBox(RefPress game) {
+        textureAtlas = new TextureAtlas(Gdx.files.internal(checkBoxImg));
+        textureRegion = textureAtlas.findRegion("checkBoxChecked");
+
+        this.game = game;
+    }
+
+
 
 }
