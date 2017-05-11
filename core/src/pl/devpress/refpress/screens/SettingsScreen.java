d65be13f23fb482deceeadf41e4dc35d22721fcb
BartoszDabek <bartek50600@wp.pl>
2017-05-11 19:45:34 +0200

    added text 'mute sound' to settings screen

--- core/src/pl/devpress/refpress/screens/SettingsScreen.java
+++ core/src/pl/devpress/refpress/screens/SettingsScreen.java
@@ -1,8 +1,15 @@
 package pl.devpress.refpress.screens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
 import pl.devpress.refpress.RefPress;
 import pl.devpress.refpress.ui.BackToMenu;
-import pl.devpress.refpress.ui.CheckBox;
+import pl.devpress.refpress.ui.MuteSoundButton;
 import pl.devpress.refpress.ui.MinusButton;
 import pl.devpress.refpress.ui.PlusButton;
 
@@ -11,7 +18,11 @@ public class SettingsScreen extends AbstractScreen {
 	private MinusButton soundDown;
 	private PlusButton plusButton;
 	private BackToMenu backToMenu;
-	private CheckBox checkBox;
+	private MuteSoundButton checkBox;
+	private FreeTypeFontGenerator generator;
+	private FreeTypeFontParameter parameter;
+	private BitmapFont textFont;
+	private String text = "MUTE SOUND:";
 	
 	public SettingsScreen(RefPress game) {
 		super(game);
@@ -19,6 +30,7 @@ public class SettingsScreen extends AbstractScreen {
 
 	@Override
 	protected void init() {
+		
 		initSoundDown();
 		initPlusButton();
 		initBackToMenu();
@@ -27,10 +39,19 @@ public class SettingsScreen extends AbstractScreen {
 		stage.addActor(plusButton);
 		stage.addActor(backToMenu);
 		stage.addActor(checkBox);
+
+		generator = new FreeTypeFontGenerator(Gdx.files.internal("OpenSans-ExtraBold.ttf"));
+		parameter = new FreeTypeFontParameter();
+		
+		parameter.size = 34;
+		parameter.borderColor = Color.BLACK;
+		parameter.borderWidth = 2;
+		
+		textFont = generator.generateFont(parameter);
 	}
 
 	private void initCheckBox() {
-		checkBox = new CheckBox();
+		checkBox = new MuteSoundButton();
 	}
 
 	private void initBackToMenu() {
@@ -49,10 +70,15 @@ public class SettingsScreen extends AbstractScreen {
 	@Override
 	public void render(float delta) {
 		super.render(delta);
+
+		spriteBatch.begin();
+		textFont.draw(spriteBatch, text, 5, 425);	
+		spriteBatch.end();
 		
 		spriteBatch.begin();
 		stage.draw();
 		spriteBatch.end();
+		
 	}
 	
 	@Override
